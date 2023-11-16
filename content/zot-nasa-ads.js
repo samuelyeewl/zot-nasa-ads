401a62ab86127170ee3c8f6a93e5f5730a3ef9be
Samuel <samuelyeewl@gmail.com>
2023-11-15 21:05:13 -0500

    Added functionality to retrieve PDFs.

--- content/zot-nasa-ads.js
+++ content/zot-nasa-ads.js
@@ -63,7 +63,7 @@ if (doi) {
 // If neither a DOI nor an arXiv ID is available, alert the user and exit
 if (!queryValue) {
   Zotero.alert(null, "Zot-NASA-ADS", "No identifier (DOI or arXiv ID) found for the selected item.");
-    return;
+  return;
 }
 
 // Define the URL for the NASA ADS API.
@@ -119,11 +119,11 @@ try {
 
     // Strip the day from the date if it's not available.
     if (adsData.pubdate) {
-    let formattedPubDate = adsData.pubdate;
-    if (formattedPubDate.endsWith("-00")) {
-        formattedPubDate = formattedPubDate.substring(0, formattedPubDate.length - 3);
-    }
-    item.setField('date', formattedPubDate);
+        let formattedPubDate = adsData.pubdate;
+        if (formattedPubDate.endsWith("-00")) {
+            formattedPubDate = formattedPubDate.substring(0, formattedPubDate.length - 3);
+        }
+        item.setField('date', formattedPubDate);
     }
     if (adsData.issn) item.setField('ISSN', adsData.issn[0]);
     if (adsData.doi) item.setField('DOI', adsData.doi[0]);
@@ -137,8 +137,8 @@ try {
     if (adsData.property.includes('EPRINT_OPENACCESS')) {
         let arxivId = adsData.identifier.find(id => id.startsWith('arXiv:'));
         if (arxivId) {
-        newExtraLines.push(`tex.archivePrefix: arXiv`);
-        newExtraLines.push(`tex.eprint: ${arxivId.replace('arXiv:', '')}`);
+            newExtraLines.push(`tex.archivePrefix: arXiv`);
+            newExtraLines.push(`tex.eprint: ${arxivId.replace('arXiv:', '')}`);
         }
         if (adsData.arxiv_class) newExtraLines.push(`tex.primaryClass: ${adsData.arxiv_class[0]}`);
     }
@@ -153,4 +153,117 @@ try {
 } catch (error) {
     Zotero.alert(null, "Zot-NASA-ADS", "An error occurred: " + error.message);
 }
+};
+
+Zotero.ZotNasaAds.updatePdfFromNasaAds = async function() {
+    // Check for the API key, and prompt the user if it's not set.
+    let apiKey = Zotero.ZotNasaAds.getApiKey();
+    if (!apiKey) {
+    Zotero.ZotNasaAds.promptForApiKey();
+    apiKey = Zotero.ZotNasaAds.getApiKey();
+    // If the user cancels the prompt, apiKey will be null or empty.
+    if (!apiKey) {
+        Zotero.alert(null, "Zot-NASA-ADS", "Operation cancelled. NASA ADS API key is required.");
+        return;
+    }
+    }
+
+    // Get the selected item in Zotero.
+    let selectedItems = Zotero.getActiveZoteroPane().getSelectedItems();
+    if (selectedItems.length != 1) {
+        Zotero.alert(null, "Zot-NASA-ADS", "Please select one item.");
+        return;
+    }
+
+    let item = selectedItems[0];
+    let doi = item.getField('DOI');
+    let queryValue = '';
+
+    // If a DOI is available, use it for the query
+    if (doi) {
+    queryValue = `doi:${encodeURIComponent(doi)}`;
+    } else {
+    // If no DOI is available, check the "Archive ID" field for an arXiv ID
+    let identifier = item.getField('archiveID') || '';
+    // If the "Archive ID" field doesn't contain an arXiv ID, try to parse the "URL" field
+    if (!identifier.includes('arXiv:')) {
+        let url = item.getField('url');
+        let arxivMatch = url.match(/arxiv\.org\/abs\/([0-9\.]+)/i);
+        if (arxivMatch) {
+        identifier = 'arXiv:' + arxivMatch[1];
+        }
+    }
+    // Use the arXiv ID for the query if it's available
+    if (identifier.includes('arXiv:')) {
+        queryValue = identifier;
+    }
+    }
+
+    // If neither a DOI nor an arXiv ID is available, alert the user and exit
+    if (!queryValue) {
+    Zotero.alert(null, "Zot-NASA-ADS", "No identifier (DOI or arXiv ID) found for the selected item.");
+    return;
+    }
+    
+    let nasaAdsApiUrl = `https://api.adsabs.harvard.edu/v1/search/query?q=${queryValue}&fl=esources,bibcode&rows=1`;
+
+    try {
+        let response = await Zotero.HTTP.request("GET", nasaAdsApiUrl, {
+        headers: {
+            "Authorization": "Bearer " + apiKey
+        }
+        });
+
+        if (response.status !== 200) {
+        Zotero.alert(null, "Zot-NASA-ADS", "Failed to fetch data from NASA ADS.");
+        return;
+        }
+
+        let data = JSON.parse(response.responseText);
+        if (data.response.docs.length === 0 || !data.response.docs[0].esources.includes('PUB_PDF')) {
+        Zotero.alert(null, "Zot-NASA-ADS", "Published PDF is not available for this item.");
+        return;
+        }
+
+        // Construct the PDF download URL
+        let pdfUrl = `https://ui.adsabs.harvard.edu/link_gateway/${data.response.docs[0].bibcode}/PUB_PDF`;
+
+        Zotero.debug(pdfUrl)
+
+        // Download and attach the PDF
+        // await Zotero.Attachments.addPDFFromURLs(item, [pdfUrl])
+        await Zotero.Attachments.importFromURL({
+            libraryID: item.libraryID,
+            parentItemID: item.id,
+            url: pdfUrl,
+            contentType: 'application/pdf',
+            referrer: `https://ui.adsabs.harvard.edu/`
+        })
+
+        // // Download the PDF
+        // let pdfResponse = await Zotero.HTTP.request("GET", pdfUrl, {
+        // headers: {
+        //     "Authorization": "Bearer " + apiKey
+        // },
+        // responseType: 'blob'
+        // });
+
+        // if (pdfResponse.status !== 200) {
+        // Zotero.alert(null, "Zot-NASA-ADS", "Failed to download PDF from NASA ADS.");
+        // return;
+        // }
+
+        // // Attach the PDF to the Zotero item
+        // let pdfBlob = pdfResponse.response;
+        // let pdfFile = new File([pdfBlob], "publication.pdf", { type: "application/pdf" });
+        // await Zotero.Attachments.linkFromFile({
+        // file: pdfFile,
+        // parentItem: item.id
+        // });
+
+        Zotero.alert(null, "Zot-NASA-ADS", "PDF downloaded and attached to the Zotero item.");
+
+    } catch (error) {
+        Zotero.alert(null, "Zot-NASA-ADS", "An error occurred: " + error.message);
+    }
 };
